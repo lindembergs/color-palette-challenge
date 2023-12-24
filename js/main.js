abd43f990e5e105f8843bc4eb3a0702e024eb5b8
Lindemberg <lindembergparaiba@gmail.com>
2023-12-24 13:43:39 -0300

    criacao da funcao que embaralha as paletas

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ function shuffleArray(array) {
   }
   return array;
 }
+function shuffleColors() {
+  const scrambledColors = shuffleArray([...originalColors]);
+  showColors(scrambledColors);
+  console.log(scrambledColors);
+}
 // criando uma função para buscar na "api" e carregar a paleta de acordo com o index random do Array, .then utiliza promisses, correto? também é possivel utilizar async await, correto?.
 function loadRandomPalette() {
   fetch("../data.json")
