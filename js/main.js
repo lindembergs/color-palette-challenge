145772bed1b36d10fbc8a9aef08d1975834400b2
Lindemberg <lindembergparaiba@gmail.com>
2023-12-24 11:48:44 -0300

    criada a promise para obter os dados da api

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-let colorPalette = getData();
-
-async function getData() {
-  const response = await fetch("../data.json");
-  console.log(response);
-  const data = await response.json();
-  console.log(data);
+let originalColors = [];
+// criando uma função para buscar na "api" e carregar a paleta de acordo com o index random do Array, .then utiliza promisses, correto? também é possivel utilizar async await, correto?.
+function loadRandomPalette() {
+  fetch("../data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const palettes = data;
+      console.log(palettes);
+    });
 }
-getData();
+
+loadRandomPalette();
